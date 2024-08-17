88de8c9aa6edb95c8aa1ce55386a265e90e96ef6
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-08-17 09:20:58 +0530

    update

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -2,22 +2,28 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { getPostDataInclude } from "@/lib/types";
 import { createPostSchema } from "@/lib/validation";
 
-export async function submitPost(input: string) {
+export async function submitPost(input: {
+  content: string;
+  mediaIds: string[];
+}) {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { content } = createPostSchema.parse({ content: input });
+  const { content, mediaIds } = createPostSchema.parse(input);
 
   const newPost = await prisma.post.create({
     data: {
       content,
       userId: user.id,
+      attachments: {
+        connect: mediaIds.map((id) => ({ id })),
+      },
     },
-    include: postDataInclude,
+    include: getPostDataInclude(user.id),
   });
 
   return newPost;
