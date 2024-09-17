07d800dd8e24ac6f0778b0af24e8cf5e1960ab52
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-09-17 20:37:59 +0530

    update

--- script.groovy
+++ script.groovy
@@ -1,3 +1,5 @@
+@Library('your-shared-library') _ // If using shared library
+
 pipeline {
     agent any
     tools {
@@ -133,3 +135,36 @@ VERCEL_TOKEN=${VERCEL_TOKEN}
     }
 }
 
+def versionIncrement() {
+    echo "Incrementing the app version"
+
+    // Read the current version from package.json
+    def packageJson = readJSON file: 'package.json'
+    def currentVersion = packageJson.version
+
+    // Parse the version components
+    def (major, minor, patch) = currentVersion.tokenize('.')
+
+    // Increment the patch version
+    def newPatch = patch.toInteger() + 1
+    def newVersion = "${major}.${minor}.${newPatch}"
+
+    // Update package.json with the new version
+    bat "npm version ${newVersion} --no-git-tag-version"
+
+    echo "New Image Version Tag is ${newVersion}-${BUILD_NUMBER}"
+
+    // Return the new version tag combined with the build number
+    return "${newVersion}-${BUILD_NUMBER}"
+}
+
+def commitVersionUpdate() {
+    sshagent(['jenkins-github-ssh']) {
+        bat 'git config user.email "jenkins@ci-cd.com"'
+        bat 'git config user.name "Jenkins User"'
+        bat "git remote set-url origin git@github.com:YourUsername/your-repo-name.git"
+        bat "git add ."
+        bat 'git commit -m "Jenkins Version Bump"'
+        bat 'git push origin HEAD:main'
+    }
+}
